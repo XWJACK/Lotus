e3ed8cde8280d24bec6c74388c49f1ffe923b4a8
Jack <xuwenjiejack@gmail.com>
2017-04-02 16:53:31 +0800

    Init Project

---
+++ Sources/Request.h
@@ -0,0 +1,19 @@
+//
+//  Request.h
+//  Request
+//
+//  Created by Jack on 4/2/17.
+//
+//
+
+@import Foundation;
+
+//! Project version number for Request iOS.
+FOUNDATION_EXPORT double RequestVersionNumber;
+
+//! Project version string for Request iOS.
+FOUNDATION_EXPORT const unsigned char RequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Request/PublicHeader.h>
+
+
