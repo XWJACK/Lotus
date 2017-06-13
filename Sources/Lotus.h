f7961d6d75d10030216592b2e83a899d6c95e5ff
Jack <xuwenjiejack@gmail.com>
2017-06-13 17:16:42 +0800

    Delete useless note

--- Sources/Lotus.h
+++ Sources/Lotus.h
@@ -8,12 +8,5 @@
 
 @import Foundation;
 
-//! Project version number for Lotus iOS.
 FOUNDATION_EXPORT double LotusVersionNumber;
-
-//! Project version string for Lotus iOS.
 FOUNDATION_EXPORT const unsigned char LotusVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Lotus/PublicHeader.h>
-
-
